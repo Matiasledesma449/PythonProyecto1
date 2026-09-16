33c8040b86a2402b9f4572482b592d3808ed60d7
Matias Ledesma <matiasledesma362@gmail.com>
2026-09-15 21:30:01 -0300

    creando mi perfil Matias_Ledesma con una imagen

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Perfil
 
+
 class RegistroUsuarioForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -15,3 +16,17 @@ class PerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
         fields = ['avatar', 'fecha_nacimiento']
+        widgets = {
+            'avatar': forms.FileInput(attrs={
+                'class': 'form-control',
+                'accept': 'image/*',
+            }),
+            'fecha_nacimiento': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            }),
+        }
+        labels = {
+            'avatar': 'Avatar',
+            'fecha_nacimiento': 'Fecha de nacimiento',
+        }
